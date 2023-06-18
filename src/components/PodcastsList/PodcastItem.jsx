a70edfe20c6eae82b744a6d46f7c6f9aa6bb178a
lCodecrafter <lcodecrafter@gmail.com>
2023-06-18 13:17:57 +0200

    Add link to podcast detail to all podcasts.

--- src/components/PodcastsList/PodcastItem.jsx
+++ src/components/PodcastsList/PodcastItem.jsx
@@ -1,17 +1,20 @@
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styles from "./podcastItem.module.css";
 
-export default function PodcastItem({ imageUrl, title, author }) {
+export default function PodcastItem({ imageUrl, title, author, id }) {
   return (
     <li className={styles.podcast}>
-      <div className={styles.imageContainer}>
-        <img src={imageUrl} alt={`${title}`} />
-      </div>
-      <div className={styles.content}>
-        <span className={styles.title}>{title}</span>
-        <br />
-        <span className={styles.author}>{author}</span>
-      </div>
+      <Link to={`/podcast/${id}`} className={styles.link}>
+        <div className={styles.imageContainer}>
+          <img src={imageUrl} alt={`${title}`} />
+        </div>
+        <div className={styles.content}>
+          <span className={styles.title}>{title}</span>
+          <br />
+          <span className={styles.author}>{author}</span>
+        </div>
+      </Link>
     </li>
   );
 }
@@ -20,4 +23,5 @@ PodcastItem.propTypes = {
   imageUrl: PropTypes.string,
   title: PropTypes.string,
   author: PropTypes.string,
+  id: PropTypes.string,
 };
